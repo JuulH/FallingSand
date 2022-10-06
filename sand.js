67bf1a698468075c8465b36e4a06af28ef963f80
juulh <juul@sharks.nl>
2022-10-06 22:43:40 +0200

    Draw function
    Implemented a draw function that can be used to draw r,g,b values at any given coordinate on the canvas. Easier for more complicated rendering & use in other functions.

--- sand.js
+++ sand.js
@@ -11,25 +11,26 @@ const ctx = canvas.getContext('2d');
 
 buffer = ctx.getImageData(0,0, canvas.width, canvas.height);
 
-function draw() {
-    for (i = 0; i < buffer.data.length; i += 4) {
-        let r = Math.random() * 255;
-        let g = Math.random() * 255;
-        let b = Math.random() * 255;
+function draw(x, y, r, g, b) {
+    let i = x * 4 + y * 4 * canvas.width;  // Coordinate
+    buffer.data[i] = r;                    // Red
+    buffer.data[i + 1] = g;                // Green
+    buffer.data[i + 2] = b;                // Blue
+    buffer.data[i + 3] = 255;              // Alpha
+}
 
-        buffer.data[i] = r;
-        buffer.data[i + 1] = g;
-        buffer.data[i + 2] = b;
-        buffer.data[i + 3] = 255;
+for (y = 0; y < canvas.height; y++) {
+    for (x = 0; x < canvas.width; x++) {
+        let c = (x + y) / 4;
+        draw(x, y, ...[c,c,c]);
     }
 }
 
-draw();
 function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);   // Clear frame
 
-    ctx.putImageData(buffer, 0, 0);
+    ctx.putImageData(buffer, 0, 0);                     // Draw buffer to screen
 }
 
 animate();

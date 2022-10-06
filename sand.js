19a7d5bce9378eefbb64863260a1a3f38b449d75
juulh <juul@sharks.nl>
2022-10-06 21:02:22 +0200

    Frame buffer
    Frame buffer implementation using canvas ImageData type

--- sand.js
+++ sand.js
@@ -4,8 +4,32 @@
 
 const canvas = document.getElementById('canvas');
 
+canvas.width = 512;
+canvas.height = 512;
+
 const ctx = canvas.getContext('2d');
 
-ctx.fillRect(100, 20, 100, 100);
+buffer = ctx.getImageData(0,0, canvas.width, canvas.height);
 
-console.log('test');
+function draw() {
+    for (i = 0; i < buffer.data.length; i += 4) {
+        let r = Math.random() * 255;
+        let g = Math.random() * 255;
+        let b = Math.random() * 255;
+
+        buffer.data[i] = r;
+        buffer.data[i + 1] = g;
+        buffer.data[i + 2] = b;
+        buffer.data[i + 3] = 255;
+    }
+}
+
+draw();
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.putImageData(buffer, 0, 0);
+}
+
+animate();
